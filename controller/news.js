7aec5fac45af18ce72c227918bc3f4a4609aa7df
amber phillips <amberphillips@ambers-MBP.fios-router.home>
2018-06-14 22:24:33 -0500

    handlebars

--- controller/news.js
+++ controller/news.js
@@ -8,51 +8,12 @@ app.get("/", function(req,res) {
   res.render("index");
 })
 
-
-//var db = require ("./models");
-
-app.get("/scrape", function(req,res) {
+app.get("/articles"), function(req, res) {
  
-axios.get("https://www.npr.org/").then(function(response){
-        
-  var $ = cheerio.load(response.data);
-
-    $("article h3").each(function(i,element) {
+  db.article.find({})
+   .then(function(dbarticle) {
       
-  var result = {};
-
-        result.title = $(this)
-        .children("")
-         .text();
-        result.link = $(this)
-         .children("")
-         .attr("href");
-
-         results.push({
-          title: title,
-          link: link
-        });
-      });
-      
-         db.article.create(result)
-          .then(function(dbarticle) {
-            console.log(dbarticle);
-          })
-           .catch(function(err) {
-             return res.json(err);
-           });
-          });  
-          res.send("scrape complete");
-        });
-
-    
-
-     app.get("/articles"), function(req, res) {
- 
-       db.article.find({})
-        .then(function(dbarticle) {
-      
-        res.json(dbarticle);
+   res.json(dbarticle);
 
     })
     .catch(function(err) {
@@ -62,38 +23,58 @@ axios.get("https://www.npr.org/").then(function(response){
       });
     }
 
-    app.get ("/articles/:id", function(req,res) {
-
-     db.article.findone({ _id: req.params.id})
-
-      .populate("comments")
-      .then(function(dbarticle) {
-
-      res.json(dbarticle);
-    })
-     .catch(function(err) {
-
-      res.json(err);
-
-   });
-  });
-
-   app.post("/articles/:id", function(req,res) {
-
-    db.article.create(req.body)
-     .then(function(dbcomments) {
-
-       return db.article.findOneAndUpdate({_id: req.params.id}, { comments: dbcomments._id}, { new:true});
-     })
-     .then(function(dbarticle) {
-
-      res.json(dbarticle);
-
-     })
-     .catch(function(err) {
-
-      res.json(err);
+    app.get("/scrape", function(req,res) {
+ 
+      axios.get("https://www.npr.org/").then(function(response){
+              
+        var $ = cheerio.load(response.data);
+      
+          $("article h3").each(function(i,element) {
+            
+        var result = {};
+      
+              result.title = $(this)
+              .children("")
+               .text();
+              result.link = $(this)
+               .children("")
+               .attr("href");
+      
+               results.push({
+                title: title,
+                link: link
+              });
+            });
+            
+       db.article.create(result)
+         .then(function(dbarticle) {
+           console.log(dbarticle);
+       })
+          .catch(function(err) {
+        return res.json(err);
      });
-   });
+   });  
+      res.send("scrape complete");
+    });
+
+    app.get("articles/:id", function(req,res) {
+      db.Comments.create(req,body)
+
+      .then (function(dbcomment) {
+        res.json(dbArticle)
+      })
+    });
+
+
+    app.post("/articles/:id", function(req, res) {
+      // Create a new note and pass the req.body to the entry
+      db.Note.create(req.body)
+        .then(function(dbNote) {
+      
+          res.json(dbArticle);
+        })
+      });
+
 
    exports = module.exports = app;
+
